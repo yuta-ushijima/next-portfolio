e396fe79ae7af6aebc7a0c1fb20d2d67f2a01638
Yuta_Ushizima <contact@yuta-u.com>
2021-09-12 23:28:37 +0900

    feat: Implement getStaticProps with testText

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,4 +1,15 @@
-const Blog = () => {
+const Blog = (props) => {
+  console.log(props);
   return <h1>ブログページ</h1>;
 };
 export default Blog;
+
+export async function getStaticProps() {
+  const testText = "Next.jsポートフォリオ";
+
+  return {
+    props: {
+      test: testText,
+    },
+  };
+}
