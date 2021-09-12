41eb00512649f58df116684c4bc65886d37add26
Yuta_Ushizima <contact@yuta-u.com>
2021-09-12 21:05:40 +0900

    feat: Crate link to contact page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,14 @@
+import Link from "next/link";
 import * as style from "../styles/index.module.css";
 
 const Index = () => {
-  return <h1 className={style.h1Text}>こんにちは</h1>;
+  return (
+    <div>
+      <h1 className={style.h1Text}>こんにちは</h1>
+      <Link href="/contact">
+        <a>Contactページへ移動</a>
+      </Link>
+    </div>
+  );
 };
 export default Index;
