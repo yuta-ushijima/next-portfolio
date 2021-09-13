7cab23fca774f9898930b0b60f496c4fb836a4ec
Yuta_Ushizima <contact@yuta-u.com>
2021-09-13 20:03:07 +0900

    feat: Create each blog article url

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,3 +1,5 @@
+import matter from "gray-matter";
+
 const Blog = (props) => {
   console.log(props);
   return <h1>ブログページ</h1>;
@@ -9,9 +11,20 @@ export async function getStaticProps() {
     const keys = context.keys();
     const values = keys.map(context);
     console.log(keys);
-    console.log(values);
+    const data = keys.map((key, index) => {
+      let slug = key.replace(/^.*[\]\\/]/, "").slice(0, -3);
+      const value = values[index];
+      const document = matter(value.default);
+
+      return {
+        frontmatter: document.data,
+        slug: slug,
+      };
+    });
+    return data;
   })(require.context("../data", true, /\.md$/));
 
+  console.log(blogs);
   return {
     props: {},
   };
