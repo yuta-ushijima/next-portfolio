93eb5cf80aeb97b34e7b1d2d624f823a38f22dbe
Yuta_Ushizima <contact@yuta-u.com>
2021-09-14 00:00:43 +0900

    feat: Create props of paths for generate each blog article url

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -3,3 +3,21 @@ const SingleBlog = () => {
 };
 
 export default SingleBlog;
+
+export async function getStaticPaths() {
+  const blogSlugs = ((context) => {
+    const keys = context.keys;
+    const values = keys.map((key, index) => {
+      let slug = key.replace(/^.*[\\\/]/).slice(0, -3);
+      return slug;
+    });
+    return data;
+  })(require.context("../data", true, /\.md$/));
+
+  const paths = blogSlugs.map((blogSlug) => `/blog/${blogSlug}`);
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
