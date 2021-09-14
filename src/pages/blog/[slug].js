7fb116c8311ed05c0b388931974a48565ec51b7d
Yuta_Ushizima <contact@yuta-u.com>
2021-09-14 23:51:37 +0900

    fix: Error and Check singleDucument value on console.log

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -1,3 +1,5 @@
+import matter from "gray-matter";
+
 const SingleBlog = () => {
   return <h1>記事ページ</h1>;
 };
@@ -6,18 +8,28 @@ export default SingleBlog;
 
 export async function getStaticPaths() {
   const blogSlugs = ((context) => {
-    const keys = context.keys;
-    const values = keys.map((key, index) => {
-      let slug = key.replace(/^.*[\\\/]/).slice(0, -3);
+    const keys = context.keys();
+    const data = keys.map((key, index) => {
+      let slug = key.replace(/^.*[\\\/]/, "").slice(0, -3);
       return slug;
     });
     return data;
-  })(require.context("../data", true, /\.md$/));
+  })(require.context("../../data", true, /\.md$/));
 
   const paths = blogSlugs.map((blogSlug) => `/blog/${blogSlug}`);
 
   return {
     paths: paths,
     fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { slug } = context.params;
+  const data = await import(`../../data/${slug}.md`);
+  const singleDocument = matter(data.default);
+  console.log(singleDocument);
+  return {
+    props: {},
   };
 }
