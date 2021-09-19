0314044d0bda9c1a9d7c9c1007aaf1f675ba1e42
Yuta_Ushizima <contact@yuta-u.com>
2021-09-19 11:31:37 +0900

    feat: Create Layout Component

---
+++ src/components/layout.js
@@ -0,0 +1,13 @@
+import Header from "./header";
+import Footer from "./footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
