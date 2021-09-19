13e621c22a85578603200114758c76d22f76accf
Yuta_Ushizima <contact@yuta-u.com>
2021-09-19 11:07:15 +0900

    fix: Contact Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,28 @@
 const Contact = () => {
-  return <h1>コンタクトページ</h1>;
+  return (
+    <>
+      <div>
+        <div>
+          <h1>Contact</h1>
+          <p>お気軽にお問い合わせください</p>
+          <form>
+            <label htmlFor="name">お名前</label>
+            <input type="text" name="name" id="name" required />
+            <label htmlFor="email">メールアドレス</label>
+            <input type="email" name="email" id="email" required />
+            <label htmlFor="textarea">ご用件</label>
+            <textarea
+              name="message"
+              rows="10"
+              id="textarea"
+              required
+            ></textarea>
+            <button type="submit">送信</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
